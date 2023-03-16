78d3892f5e9f538f4bc4ec15b6d011a6bc6c15e8
dawit Yenew <dawityenew.12@gmail.com>
2023-03-16 15:20:23 -0700

    commi tthe last problem

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- * main - Entry point
+ * main- Entry point
  *
- * Return: Always 0 (Success)
+ * Description: A program that prints to standard error
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-        write(1, "and that piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
-
