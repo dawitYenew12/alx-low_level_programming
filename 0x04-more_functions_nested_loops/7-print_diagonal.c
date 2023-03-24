5c0788cdf0166614b4366d09b6d50a5b79981bc1
dawit Yenew <dawityenew.12@gmail.com>
2023-03-24 02:13:52 -0700

    debug

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,21 +9,19 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_putchar("\n");
-	}
-	else
+		_putchar('\n');
+	} else
 	{
-		int i;
-		int j;
+		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-					_putchar("\\");
-				else if (j < 1)
-					_putchar('');
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
 			_putchar('\n');
 		}
