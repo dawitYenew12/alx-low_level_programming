8f0d3adbaefe17ea6bd920f94276b5f8055268d0
dawit Yenew <dawityenew.12@gmail.com>
2023-03-24 00:58:05 -0700

    commiting

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * more_numbers - Print numbers between 0 to 14 incl.
@@ -15,15 +14,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			int tens;
-			int ones;
-
-			tens = j / 10;
-			ones = j % 10;
-
-			if (j > 0)
-				_putchar(tens + '0');
-			_putchar(ones + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
