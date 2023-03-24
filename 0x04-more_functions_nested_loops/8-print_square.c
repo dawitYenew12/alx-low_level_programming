95e4f9649a3ab1a4d868c6ced54b308606b53826
dawit Yenew <dawityenew.12@gmail.com>
2023-03-24 01:22:23 -0700

    debugging

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,16 +11,19 @@ void print_square(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			_putchat('#');
-		}
-		_putchar('\n');
-	}
 	if (n <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
