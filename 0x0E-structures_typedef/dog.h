b9808d0da2d6374e810502296554218bb1756486
dawitYenew12 <dawitYenew.12@gmail.com>
2023-04-17 13:48:07 +0300

    commiting readme and problem 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+/**
+ * Struct dog - this datatype consists name,age and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+Struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
